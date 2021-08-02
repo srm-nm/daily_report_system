112cb1422f72ab6ecdc3e61a77f746967c6d4bc7
ryo.sato <srm09.sh@gmail.com>
2021-08-02 17:45:09 +0900

    日報showアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -90,10 +90,10 @@ public class ReportAction extends ActionBase {
      */
     public void create() throws ServletException, IOException {
 
-        //CSRF対策 tokenのチェック
+        // CSRF対策 tokenのチェック
         if (checkToken()) {
 
-            //日報の日付が入力されていなければ、今日の日付を設定
+            // 日報の日付が入力されていなければ、今日の日付を設定
             LocalDate day = null;
             if (getRequestParam(AttributeConst.REP_DATE) == null
                     || getRequestParam(AttributeConst.REP_DATE).equals("")) {
@@ -102,10 +102,10 @@ public class ReportAction extends ActionBase {
                 day = LocalDate.parse(getRequestParam(AttributeConst.REP_DATE));
             }
 
-            //セッションからログイン中の従業員情報を取得
+            // セッションからログイン中の従業員情報を取得
             EmployeeView ev = (EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
 
-            //パラメータの値をもとに日報情報のインスタンスを作成する
+            // パラメータの値をもとに日報情報のインスタンスを作成する
             ReportView rv = new ReportView(
                     null,
                     ev, //ログインしている従業員を、日報作成者として登録する
@@ -115,28 +115,51 @@ public class ReportAction extends ActionBase {
                     null,
                     null);
 
-            //日報情報登録
+            // 日報情報登録
             List<String> errors = service.create(rv);
 
             if (errors.size() > 0) {
-                //登録中にエラーがあった場合
+                // 登録中にエラーがあった場合
 
                 putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
                 putRequestScope(AttributeConst.REPORT, rv);//入力された日報情報
                 putRequestScope(AttributeConst.ERR, errors);//エラーのリスト
 
-                //新規登録画面を再表示
+                // 新規登録画面を再表示
                 forward(ForwardConst.FW_REP_NEW);
 
             } else {
-                //登録中にエラーがなかった場合
+                // 登録中にエラーがなかった場合
 
-                //セッションに登録完了のフラッシュメッセージを設定
+                // セッションに登録完了のフラッシュメッセージを設定
                 putSessionScope(AttributeConst.FLUSH, MessageConst.I_REGISTERED.getMessage());
 
-                //一覧画面にリダイレクト
+                // 一覧画面にリダイレクト
                 redirect(ForwardConst.ACT_REP, ForwardConst.CMD_INDEX);
             }
+        }
+    }
+
+    /**
+     * 詳細画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void show() throws ServletException, IOException {
+
+        // idを条件に日報データを取得する
+        ReportView rv = service.findOne(toNumber(getRequestParam(AttributeConst.REP_ID)));
+
+        if (rv == null) {
+            // 該当の日報データが存在しない場合はエラー画面を表示
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+
+        } else {
+
+            putRequestScope(AttributeConst.REPORT, rv); //取得した日報データ
+
+            // 詳細画面を表示
+            forward(ForwardConst.FW_REP_SHOW);
         }
     }
 }
